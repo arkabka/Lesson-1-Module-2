fe074b734b5ea48c6b9f3430f8f30f8259944776
аркабека <@234_5>
2018-05-24 12:00:55 +0600

    24.05

--- ConsoleApplication3/Source.cpp
+++ ConsoleApplication3/Source.cpp
@@ -6,91 +6,229 @@
 void main()
 
 {
-	//printf("a) b5 10 \tb) 100 t \tc) x 25\n   7 cm \t   1949 v \t   x y\n");
-
-	//Задание 2
-
-	/*setlocale(LC_ALL, "Rus");
-
-	float a, y;
-	printf("Введите значение a ");
-
-	scanf("%f", &a);
-
-	y = (7 * a*a) - (3 * a) + 3;
-
-	printf("Значение У = %f\n", y);*/
-
-	// Задание 3
-
-	/*int a, x;
-
-	printf("Ukazhites storonu kvadrata x = ");
-	scanf("%d", &x);
-
-	a = x * 4;
-
-	printf("Znachenie a = %d\n", a);*/
-
-	// Задание 4
-
-	/*float r, p;
-	printf("Ukazhite radius okruzhnosti r = ");
-	scanf("%f", &r);
-
-	p = r * 2 * 3.14;
-
-	printf("%f\n", p);*/
-
-	
-
 	setlocale(LC_ALL, "Rus");
 
-
 	int n;
-	printf("Выберите номер задания n = ");
-	scanf("%d", &n);
+	printf("Please choose the number of exercise n = ");
+	scanf_s("%d", &n);
+
+
 	if (n == 1)
 	{
-		float M, S, X, Y, Z;
-		printf(" 1.Известно, что X кг конфет стоит A рублей. Определить, сколько стоит 1 кг и Y кг этих же конфет.\n Введите массу в кг = ");
-		scanf("%f", &M);
-		printf(" Введите стоимость = ");
-		scanf("%f", &S);
-		printf(" Введите массу стоимость которой Вас интересует = ");
-		scanf("%f", &Y);
+		int a;
+		printf("Please input any number = ");
+		scanf_s("%d", &a);
 
-		X = S / M;
-		Z = X * Y;
-
-		printf("Стоимость одного киллограма составляет  = %f\n Стоимость интересующей Вас массы = %f\n", X, Z); 
-	}  
-	if (n == 2)
-	{
-		float A, B, x;
-	start:
-
-
-		printf("2.Решить линейное уравнение A·x + B = 0, заданное своими коэффициентами A и B (коэффициент A не равен 0).\nВведите, пожалуйста, значение для А = ");
-		scanf("%f", &A);
-		printf("Введите, пожалуйста6, значение для B = ");
-		scanf("%f", &B);
-
-		if (A == 0)
+		if (a >= 0)
 		{
-			printf("Значение А не должно быть равно 0\n");
-			goto start;
+			printf("The number you wrote is possitive\n");
 		}
-
-		x = -(B / A); 
-		printf("Значение X равно = %f\n", x);
-
-
+		else
+		{
+			printf("The number you wrote is negative\n");
+		}
 	}
-	if (n == 3)
+	else if (n == 2)
 	{
-		
+
+		int a;
+		printf("Please input any number = ");
+		scanf_s("%d", &a);
+
+		if ((a % 2) == 0)
+		{
+			printf("The number you wrote is even\n");
+		}
+		else
+		{
+			printf("The number you wrote is add\n");
+		}
 	}
+	else if (n == 3)
+	{
+		int a, b;
+		printf("Please input any number for a and b ");
+		scanf_s("%d%d", &a, &b);
 
+		if ((a % 2) == 0)
+		{
+			printf("The number 'a' is even\n");
+		}
+		else
+		{
+			printf("The number 'a' is add\n");
+		}
+	}
+	else if (n == 4)
+	{
+		int a, b;
+		printf("Please input numbers for a and b ");
+		scanf_s("%d%d", &a, &b);
 
+		if ((a > 2) && (b <= 3))
+		{
+			printf("True inequalities A>2 and B<=3\n");
+		}
+		else
+		{
+			printf("False inequalities A>2 and B<=3\n");
+		}
+	}
+	else if (n == 5)
+	{
+		int a, b;
+		printf("Please input numbers for a and b ");
+		scanf("%d%d", &a, &b);
+
+		if ((a > 0) && (b < -2))
+		{
+			printf("True inequalities A>0 and B<-2\n");
+		}
+		else
+		{
+			printf("False inequalities A>0 and B<-2\n");
+		}
+	}
+	else if (n == 6)
+	{
+		int a, b, c;
+		printf("Please input numbers for a b c ");
+		scanf("%d%d%d", &a, &b, &c);
+
+		if ((a < b) && (b < c))
+		{
+			printf("True inequalities for a<b<c\n ");
+		}
+		else
+		{
+			printf("False inequalities for a<b<c\n");
+		}
+	}
+	else if (n == 7)
+	{
+		int a, b, c;
+		printf("Please input numbers for a b c ");
+		scanf("%d%d%d", &a, &b, &c);
+
+		if ((a < b) && (b < c))
+		{
+			printf("True inequalities for 'number b is between a & c'\n");
+		}
+		else
+		{
+			printf("False inequalities for 'number b is between a & c'\n");
+		}
+	}
+	else if (n == 8)
+	{
+		int a, b;
+		printf("Please input numbers for a and b ");
+		scanf("%d%d", &a, &b);
+
+		if (((a % 2) == 0) && ((b % 2) == 0))
+		{
+			printf("Each number is even\n");
+		}
+		else
+		{
+			printf("Each number is add\n");
+		}
+	}
+	else if (n == 9)
+	{
+		int a, b;
+		printf("Please input number for a and b ");
+		scanf("%d%d", &a, &b);
+
+		if (((a % 2) == 0) || ((b % 2) == 0))
+		{
+			printf("Non of numbers is add\n");
+		}
+		else
+		{
+			printf("One or two of numbers is add\n");
+		}
+	}
+	else if (n == 10)
+	{
+		int a, b;
+		printf("Please input numbers for a and b ");
+		scanf("%d%d", &a, &b);
+
+		if ((((a % 2) == 0) && ((b % 2) >= 1)) || (((a % 2) >= 1) && ((b % 2) == 0)))
+		{
+			printf("Only one of number is add\n");
+		}
+		else
+		{
+			printf("Not only one of number is add\n");
+		}
+	}
+	else if (n == 11)
+	{
+		int a, b;
+		printf("Please input numbers for a and b ");
+		scanf("%d%d", &a, &b);
+
+		if ((((a % 2) == 0) && ((b % 2) == 0)) || (((a % 2) >= 1) && ((b % 2) >= 1)))
+		{
+			printf("Both of numbers got same parity\n");
+		}
+		else
+		{
+			printf("Both of number hasn't got same parity\n");
+		}
+	}
+	else if (n == 12)
+	{
+		int a, b, c;
+		printf("Please input numbers for a b c ");
+		scanf("%d%d%d", &a, &b, &c);
+
+		if ((a > 0) && (b > 0) && (c > 0))
+		{
+			printf("All numbers are bigger than 0 means possitive\n");
+		}
+		else
+		{
+			printf("Not number are bigger than 0 means 1 or 2 negative\n");
+		}
+	}
+	else if (n == 14)
+	{
+		int a, b, c;
+		printf("Please input numbers for a b c ");
+		scanf("%d%d%d", &a, &b, &c);
+
+		if ((a > 0) && (b < 0) && (c < 0) || (a < 0) && (b < 0) && (c > 0) || (a < 0) && (b > 0) && (c < 0))
+		{
+			printf("There are only number is possitive\n");
+		}
+		else
+		{
+			printf("There are two or more possitive numbers\n");
+		}
+	}
+	else if (n == 13)
+	{
+		int a, b, c;
+		printf("Please input numbers for a b c ");
+		scanf("%d%d%d", &a, &b, &c);
+
+		if ((a > 0) && (b < 0) && (c < 0) || (a < 0) && (b < 0) && (c > 0) || (a < 0) && (b > 0) && (c < 0) || (a > 0) && (b > 0) && (c < 0) || (a > 0) && (b < 0) && (c > 0) || (a < 0) && (b > 0) && (c > 0))
+		{
+			printf("There are at least one possitive number\n");
+		}
+		else
+		{
+			printf("There non possitive number\n");
+		}
+	}
+	else if (n == 15);
+	{
+		int 
+	}
+	
+
+	return ;
 }
